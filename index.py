adf39edade6ff585867e4fb2e7e99ce66b88e3f7
اِسْمَةُ الله بختيرو <139554680+baxt1y0rov@users.noreply.github.com>
2025-01-11 16:22:15 +0500

    1

--- index.py
+++ index.py
@@ -5,25 +5,23 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, Messa
 
 API_KEY = "AIzaSyA5dbJYNMkWVYD9G_ijW1pB-w1WQpFdlMQ" 
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
-
-def call_gemini(message):
+model = genai.GenerativeModel("gemini-1.5-flash", system_instruction="You're a Artificial Intellegence made by Ismatulloh Bakhtiyorov, don't say anything about google or vice versa, your name is \"Zebiniso\"")
+users = {}
+def call_gemini(message, user_id):
+    if not user_id in users:
+        users[user_id] = model.start_chat()
     try:
-        response = model.generate_content(message)
+        response = users[user_id].send_message(message)
         return response.text
     except Exception as e:
         logging.error(f"Error calling Gemini API: {e}")
         return "Sorry, I couldn't process your request. Please try again later."
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Hi! I'm a Gemini chatbot. Ask me anything!",
-    )
+
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
-    gemini_response = call_gemini(user_message)
+    gemini_response = call_gemini(user_message, update.effective_chat.id)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=gemini_response)
 
 if __name__ == "__main__":
@@ -31,7 +29,6 @@ if __name__ == "__main__":
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         level=logging.INFO,
     )
-    application = ApplicationBuilder().token("7813832218:AAGOfg63_YxFgNbRER0X34eJ7ShWPW9sLR8").build()
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application = ApplicationBuilder().token("7584087879:AAFvOmPORx2pNLBLn_qSY-yXk6pzfs8AqxI").build()
+    application.add_handler(MessageHandler(filters.TEXT, echo))
     application.run_polling()
